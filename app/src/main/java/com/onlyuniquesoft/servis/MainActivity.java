44921b49c565be47fc44171d3b570657c3929f97
Serkan33 <tasserkan93@gmail.com>
2016-01-11 23:11:33 +0200

    mobilbimnedir2.1

--- app/src/main/java/com/onlyuniquesoft/servis/MainActivity.java
+++ app/src/main/java/com/onlyuniquesoft/servis/MainActivity.java
@@ -1,17 +1,27 @@
 package com.onlyuniquesoft.servis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-//bismillahh
-    //baslangiccc
+Button nedir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nedir=(Button)findViewById(R.id.nedir);
+        nedir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MobileBimNedir.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
