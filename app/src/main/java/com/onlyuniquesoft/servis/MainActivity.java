cabd05a8eeb7bf59143f1bc99efc0c12fd39e949
mustafazorbaz <mustafazorbaz@hotmail.com>
2016-01-11 22:04:55 +0200

    baslangic 2.developer kullanici

--- app/src/main/java/com/onlyuniquesoft/servis/MainActivity.java
+++ app/src/main/java/com/onlyuniquesoft/servis/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 //bismillahh
-    //baslangic
+    //baslangicc
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
