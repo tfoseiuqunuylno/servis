a7fc3f5bd4835429c2044ab9a0b34b9570f14914
Serkan33 <tasserkan93@gmail.com>
2016-01-11 21:24:38 +0200

    sonrakipuslama

--- app/src/main/java/com/onlyuniquesoft/servis/MainActivity.java
+++ app/src/main/java/com/onlyuniquesoft/servis/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 //bismillahh
+    //baslangic
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
